d20ff522bd691f9700057b74912c5655856506dc
rohit <verma.rohit.1203@gmail.com>
2025-02-14 10:33:06 +0100

    Test fixing revert

--- src/test/resources/dbH2/04-insert-films.sql
+++ src/test/resources/dbH2/04-insert-films.sql
@@ -1,10 +1,3 @@
-INSERT INTO directors (name, surname, city)
-VALUES ('Christopher', 'Nolan', 'UK');
-INSERT INTO directors (name, surname, city)
-VALUES ('Mary', 'Harron', 'USA');
-INSERT INTO directors (name, surname, city)
-VALUES ('Bob', 'Marley', 'Nyc');
-
 INSERT INTO film (title, release_date, category, id_director)
 VALUES ('Inception', '2010-09-24', 'Sci-fi/Action', 1);
 INSERT INTO film (title, release_date, category, id_director)
